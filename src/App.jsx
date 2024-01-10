204d27bf73c2fad1bbd6910c8548bff49c627ca5
PC <komarovitstudies@gmail.com>
2024-01-10 12:13:57 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Player from "./componetns/Player";
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+        GAME BOARD
+      </div>
+    </main>
+  );
+}
+
+export default App;
