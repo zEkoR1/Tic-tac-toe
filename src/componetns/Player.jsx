bc78a534683173280974488652613a9508739cf2
zEkoR1 <komarovitstudies@gmail.com>
2024-01-10 13:39:20 +0200

    Working Input fields

--- src/componetns/Player.jsx
+++ src/componetns/Player.jsx
@@ -1,18 +1,25 @@
 import React, { useState } from "react";
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing((editing) => !editing);
   }
-  let playerName = <span className="player-name"> {name}</span>;
+  function handleChange(event) {
+    console.log(event);
+    setPlayerName(event.target.value);
+  }
+  let edittablePlayerName = <span className="player-name"> {playerName}</span>;
 
   if (isEditing) {
-    playerName = <input type="text" required value={name} />;
+    edittablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
   }
   return (
     <li>
       <span className="player">
-        {playerName}
+        {edittablePlayerName}
         <span className="player-symbol"> {symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
